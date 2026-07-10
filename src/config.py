1841ebe6fb93060636059b2e79c2261bdd0eea75
codefazt <jcdevelopment@gmail.com>
2026-07-10 09:59:24 -0400

    bump: actualizar versión a 2.4.4

--- src/config.py
+++ src/config.py
@@ -17,7 +17,7 @@ from datetime import datetime
 logger = logging.getLogger("SiGCABot")
 
 # Versión de la Aplicación
-APP_VERSION = "2.4.3"
+APP_VERSION = "2.4.4"
 
 # ---------------------------------------------------------------------------
 # Ofuscación / Encriptación simple de campos sensibles
